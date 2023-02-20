7ef8ad74c1d67401c077bb297dfb4e8a01f5ccaf
Siarhei Tsihovich <siarhei.tsihovich@gmail.com>
2023-02-20 16:30:13 +0400

    feat: add weather api

--- js/script.js
+++ js/script.js
@@ -1,18 +1,6 @@
 
 // time, Date, Greeting
 
-// window.onload = function () {
-//     window.setInterval(function showTime() {
-//         const date = new Date()
-//         const hours = date.getHours()
-//         const minutes = date.getMinutes()
-//         const seconds = date.getSeconds()
-//         const clock = hours + ":" + minutes + ":" + seconds;
-//         time.textContent = clock;
-//     }
-//     );
-// };
-
 const dateCalendar = document.querySelector('.date');
 const greeting = document.querySelector('.greeting');
 const time = document.querySelector('.time');
@@ -51,17 +39,17 @@ function showGreeting() {
 
 const inputName = document.querySelector('.name');
 
-function setLocalStorage() {
+function setLocalStorageName() {
     localStorage.setItem('name', inputName.value)
 }
-window.addEventListener('beforeunload', setLocalStorage);
+window.addEventListener('beforeunload', setLocalStorageName);
 
-function getLocalStorage() {
+function getLocalStorageName() {
     if (localStorage.getItem('name')) {
         inputName.value = localStorage.getItem('name')
     }
 }
-window.addEventListener('load', getLocalStorage);
+window.addEventListener('load', getLocalStorageName);
 
 //Slider
 
@@ -114,6 +102,7 @@ function getSlidePrev() {
 slideNext.addEventListener('click', getSlideNext);
 slidePrev.addEventListener('click', getSlidePrev);
 
+
 function showTime() {
     const date = new Date();
     const currentTime = date.toLocaleTimeString();
@@ -124,3 +113,60 @@ function showTime() {
     setTimeout(showTime, 1000);
 }
 showTime();
+
+//Weather
+
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
+const city = document.querySelector('.city');
+
+
+async function getWeather() {
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=4f507497abe32c4725805a4e97ff125c&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (res.ok) {
+        weatherIcon.className = 'weather-icon owf';
+        weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+        temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
+        weatherDescription.textContent = data.weather[0].description;
+        wind.textContent = `Wind speed: ${data.wind.speed}m/s`
+        humidity.textContent = `Humidity: ${data.main.humidity}%`;
+    }
+    else {
+        alert('city not found');
+    }
+}
+
+
+function setCity(event) {
+    if (event.code === 'Enter') {
+        getWeather();
+        city.blur();
+    }
+};
+// document.addEventListener('DOMContentLoaded', getWeather);
+city.addEventListener('keypress', setCity);
+
+
+
+function setLocalStorageCity() {
+    localStorage.setItem('city', city.value)
+}
+window.addEventListener('beforeunload', setLocalStorageCity);
+
+
+function getLocalStorageCity() {
+    if (localStorage.getItem('city')) {
+        city.value = localStorage.getItem('city')
+    } else { city.value = 'Minsk' }
+    getWeather(city.value)
+}
+window.addEventListener('load', getLocalStorageCity);
+
+
